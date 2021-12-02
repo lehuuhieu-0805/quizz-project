394f8666752d2f31bbb2ab58fa5ac77d973cd329
Le Huu Hieu <01643815296h@gmail.com>
2021-12-02 18:50:09 +0700

    add timesubmit for score

--- src/main/java/com/example/quizzproject/controller/ScoreController.java
+++ src/main/java/com/example/quizzproject/controller/ScoreController.java
@@ -1,13 +1,14 @@
 package com.example.quizzproject.controller;
 
 import com.example.quizzproject.model.Score;
-import com.example.quizzproject.model.User;
 import com.example.quizzproject.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -17,6 +18,11 @@ public class ScoreController {
     ScoreService scoreService;
     @PostMapping("/saveScore")
     public ResponseEntity<?> saveScore(@RequestBody Score score){
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
+        String dateFormat = formatter.format(date);
+        score.setTimeSubmit(dateFormat);
+        System.out.println(score);
         return new ResponseEntity<>(scoreService.saveScore(score), HttpStatus.CREATED);
     }
     @GetMapping("/score/{id}")
